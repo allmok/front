4d353375a83310724d0469a7648d532a5fb6e61e
allmok <159762030+allmok@users.noreply.github.com>
2024-05-13 11:21:52 +0300

    3.2
    button works on every page.

--- lab3/js/main.js
+++ lab3/js/main.js
@@ -1,33 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    handleDomContentLoaded();
-  });
-  
-  const handleDomContentLoaded = function () {
-    const body = document.body;
-    const header = document.querySelector('header');
-    const burgerBtn = document.querySelector('header .nav-btn');
-    const overlay = document.querySelector('.header .overlay');
-  
-    function handleBurgerClick() {
+const handleDomContentLoaded = function() {
+  const body = document.body;
+  const header = document.querySelector('header');
+  const burgerBtn = document.querySelector('header .nav-btn');
+  const overlay = document.querySelector('.header .overlay');
+
+  function handleBurgerClick() {
       if (header.classList.contains('is-active')) {
-        closeNavigation();
+          closeNavigation();
       } else {
-        openNavigation();
+          openNavigation();
       }
-    }
-  
-    function closeNavigation() {
+  }
+
+  function closeNavigation() {
       body.classList.remove('disable-scroll');
       header.classList.remove('is-active');
-    }
-  
-    function openNavigation() {
+  }
+
+  function openNavigation() {
       body.classList.add('disable-scroll');
       header.classList.add('is-active');
-    }
-  
-    if (burgerBtn && overlay) {
-      burgerBtn.addEventListener('click', handleBurgerClick);
-      overlay.addEventListener('click', closeNavigation);
-    }
   }
+  burgerBtn.addEventListener('click', handleBurgerClick);
+  overlay.addEventListener('click', closeNavigation);
+}
+document.addEventListener('DOMContentLoaded', handleDomContentLoaded);
